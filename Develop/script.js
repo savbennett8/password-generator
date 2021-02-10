91545d56b6c2312e6048e5e8728e3748a7250a34
savbennett5 <76716614+savbennett8@users.noreply.github.com>
2021-02-09 21:42:50 -0700

    setup 'prompts' function & linked to 'generateBtn'

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,38 @@
-// Assignment code here
+//I need arrays of all acceptable special characters
+//I need an alert for if none of the character types are selected
 
+// Assignment code here
+var prompts = function (generateBtn) {
+  var length = window.prompt("How many characters should your password have? (Between 8 - 128)");
+  if (length >= 8 && length <= 128) {
+    //I need to somehow store this value to use in the writePassword function
+  } else {
+    window.alert("Sorry, please select a number between 8 and 128.");
+    prompts();
+  };
+  
+  var lowerConfirm = window.confirm("Do you want your passwork to include lowercase letters?");
+  if (lowerConfirm) {
+    //use lowercase letters in writePassword function
+  };
+  
+  var upperConfirm = window.confirm("Do you want your password to include uppercase letters?");
+  if (upperConfirm) {
+    //use uppercase letters in writePassword function
+  };
+  
+  var numConfirm = window.confirm("Do you want your password to include numbers?");
+  if (numConfirm) {
+    //use numbers in writePassword function
+  };
+  
+  var specialConfirm = window.confirm("Do you want your password to include special characters?");
+  if (specialConfirm) {
+    //use special characters in writePassword function
+  };
+
+  //writePassword();
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -14,4 +47,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", prompts);
