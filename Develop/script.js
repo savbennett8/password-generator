df539d5c70a72cdd2781f4788beed6bdfe990892
savbennett8 <email@example.com>
2021-02-13 15:04:47 -0700

    code functioning properly!

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,8 @@
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
 //I need a variable of all acceptable characters
+
 const passKeys = {
   lowercase: 'abcedfghijklmnopqrstuvwxyz',
   uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
@@ -6,47 +10,36 @@ const passKeys = {
   symbol: '!@#$%^&*(){}[];'
 };
 
+
 //stores the confirms so they can be accesed in the writePassword function
-let promptInput = [];
+
+
 
 // Assignment code here
-var prompts = function (generateBtn) {
-  //generateBtn.preventDefault();
-  
+var prompts = function () {
   var length = window.prompt("How many characters should your password have? (Between 8 - 128)");
   if (length >= 8 && length <= 128) {
-    //push the user's input into the promptInput array
-    promptInput.push(length);
-    console.log(promptInput);
+    
+    var lowerConfirm = window.confirm("Do you want your passwork to include lowercase letters?");
+    var upperConfirm = window.confirm("Do you want your password to include uppercase letters?");
+    var numConfirm = window.confirm("Do you want your password to include numbers?");
+    var specialConfirm = window.confirm("Do you want your password to include special characters?");
+
+    generatePassword(length, lowerConfirm, upperConfirm, numConfirm, specialConfirm);
+    
+    
+    
   } else {
     window.alert("Sorry, please select a number between 8 and 128.");
     prompts();
   };
 
-  var lowerConfirm = window.confirm("Do you want your passwork to include lowercase letters?");
-  if (lowerConfirm) {
-    promptInput.push(lowerConfirm);
-    console.log(promptInput);
-  };
+}
   
-  var upperConfirm = window.confirm("Do you want your password to include uppercase letters?");
-  if (upperConfirm) {
-    promptInput.push(upperConfirm);
-    console.log(promptInput);
-  };
-  
-  var numConfirm = window.confirm("Do you want your password to include numbers?");
-  if (numConfirm) {
-    promptInput.push(numConfirm);
-    console.log(promptInput);
-  };
-  
-  var specialConfirm = window.confirm("Do you want your password to include special characters?");
-  if (specialConfirm) {
-    promptInput.push(specialConfirm);
-    console.log(promptInput);
-  };
 
+  
+let generatePassword = function(length, lowerConfirm, upperConfirm, numConfirm, specialConfirm) {
+  var passwordField = [];
   //alert for if none of the character types are selected
   if (lowerConfirm === false && upperConfirm === false && 
     numConfirm === false && specialConfirm === false) {
@@ -54,16 +47,64 @@ var prompts = function (generateBtn) {
       prompts();
   }
 
-  //writePassword();
-}
+  if (lowerConfirm) {
+    passwordField.push(passKeys.lowercase.split(""));
+    console.log('lowercase');
+  };
+  
+  if (upperConfirm) {
+    passwordField.push(passKeys.uppercase.split(""));
+    console.log('uppercase');
+  };
+  
+  if (numConfirm) {
+    passwordField.push(passKeys.number.split(""));
+    console.log('number');
+  };
+  
+  if (specialConfirm) {
+    passwordField.push(passKeys.symbol.split(""));
+    console.log('special');
+  };
+  
+  let output = '';
+ console.log(passwordField);
+  let passwordField2 = passwordField.flat();
+ 
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+  for (let i = 0; i < length; i++) {
+    output += passwordField2[Math.floor(Math.random() * passwordField2.length)];
+                     
+  }
+  passwordField = [];
+  passwordField2 = [];
+  writePassword(output);
+  
+}
+  
+  
+
+
+
+
+
+
 
 //I need the writePassword function to generate randomly
+
+  
+
+
+
+
+
+
+
+
+//var lowerRandom = passKeys.lowercase.split("")[Math.floor(Math.random() * passKeys.lowercase.length)]);
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+function writePassword(output) {
+  var password = output;
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
